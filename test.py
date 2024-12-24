e6420bdb43c9f8e4fe76fe0f572e2f976b450da1
Artur <arturunkulov30@gmail.com>
2024-12-24 18:34:45 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+
+
+
+
+############   git  , github  киришуу   #############
+
+
+#   1     git init  (ГИТТИ ИНИТСИЯЛИЗАЦИЯ КЫЛУУ)
+
+
+#   2    git add  .  /  либо по названию файла
+
+#  3     git commit -m "men registration buttum"
+
+#   4   git branch -M main
+
+#  5   git remote add origin https://github.com/Artur470/lessonpython.git
+
+#  6   git push -u origin main
